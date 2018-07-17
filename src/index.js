05af42093ca557e92c47a4f6bbada2b3e0949a1d
Gilbert Aragon <gilbertaragon7801@gmail.com>
2018-07-17 09:25:22 -0700

    added moving dot animation in the background and comments in code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function() {
+    // reference to canvas element and context to draw shapes
     var canvas = document.querySelector("canvas");
     var context = canvas.getContext('2d');
 
@@ -8,52 +9,115 @@ window.addEventListener("load", function() {
     canvas.width  = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
 
+    // reference to canvas width and height to determine dot position
     var cWidth = canvas.width;
     var cHeight= canvas.height;
-    var dotMargin = 50;
-    var numCols = 15;
-    var numRows = 15;
-    console.log("width: " + cWidth);
-    console.log("height: " + cHeight);
+    // initial direction of dot
+    var dir = ['+', '-'];
 
-    var dotWidth = ((cWidth - (2 * dotMargin)) / numCols) - dotMargin;
-    var dotHeight = ((cHeight - (2 * dotMargin)) / numRows) - dotMargin;
+    // array of dot objects
+    var dots = [];
+    create();
 
-    if( dotWidth > dotHeight ) {
-        var dotDiameter = dotHeight;
-        var xMargin = (cWidth - ((2 * dotMargin) + (numCols * dotDiameter))) / numCols;
-        var yMargin = dotMargin;
-        console.log("dotWidth larger");
-        console.log("dotWidth: " + dotWidth);
-        console.log("dotHeight: " + dotHeight);
-        console.log("xMargin: " + xMargin);
-        console.log("yMargin: " + yMargin);
-    } else {
-        var dotDiameter = dotWidth;
-        var xMargin = dotMargin;
-        var yMargin = (cHeight - ((2 * dotMargin) + (numRows * dotDiameter))) / numRows;
-        console.log("dotHeight larger");
-        console.log("dotWidth: " + dotWidth);
-        console.log("dotHeight: " + dotHeight);
-        console.log("xMargin: " + xMargin);
-        console.log("yMargin: " + yMargin);
-    }
+    // function creates 150 dot objects, populates dot array, and draws
+    // each dot on a random place on the canvas
+    function create() {
+        for (i = 0; i < 150; i++) {
+            // white color w/random opacity
+            var color = 'rgba(255,255,255,' + Math.random() + ')';
 
-    var dotRadius = dotDiameter * 0.5;
+            // direction of left or right/up or down
+            var dirNum = Math.floor(Math.random() * 2);
+            var xMove = dir[dirNum];
+            var yMove = dir[dirNum];
 
-    for(var i = 0; i < numRows; i++) { 
-        for(var j = 0; j < numCols; j++) {
-            var x = (j * (dotDiameter + xMargin)) + dotMargin + (xMargin / 2) + dotRadius;
-            var y = (i * (dotDiameter + yMargin)) + dotMargin + (yMargin / 2) + dotRadius;
-            drawDot(x, y, dotRadius);
+            // randon location on canvas
+            var x = Math.floor(Math.random() * cWidth) + (rad*2);
+            var y = Math.floor(Math.random() * cHeight) + (rad*2);
+            var speed = 0.3 * Math.random();
+            var rad = 3;
+
+            var dot = {
+                x: x,
+                y: y,
+                radius: rad,
+                xMove: xMove,
+                yMove: yMove,
+                color: color,
+                speed: speed
+            };
+
+            // push dot object to dots array and draw each dot on canvas
+            dots.push(dot);
+            drawDot(dot);
         }
-        console.log("dotRadius: " + dotRadius + "dotDiameter: " + dotDiameter);
     }
 
-    function drawDot(x, y, radius) {
-        context.beginPath();
-        context.arc(x, y, radius, 0, 2 * Math.PI, false);
-        context.fillStyle = 'rgba(255, 255, 255,' + Math.random() + ')';
-        context.fill();
+    // start animation .5 sec after page loads
+    setTimeout(function(){
+        window.requestAnimationFrame(moveDot);
+    }, 500);
+      
+    // moves dot depending on x and y direction and object's speed  
+    function moveDot() {
+        // clear canvas to redraw dots
+        context.clearRect(0, 0, cWidth, cHeight);
+      
+        for (i = 0; i < dots.length; i++) {
+            // move dot based on object's direction and speed values
+            if (dots[i].xMove == '+') {
+                dots[i].x += dots[i].speed;
+            } else {
+                dots[i].x -= dots[i].speed;
+            }
+
+            if (dots[i].yMove == '+') {
+                dots[i].y += dots[i].speed;
+            } else {
+                dots[i].y -= dots[i].speed;
+            }
+            
+            // redraw dot
+            drawDot(dots[i])
+        
+            // if dot hits edge of screen, reverse it's direction
+            if ((dots[i].x + dots[i].radius) >= cWidth) {
+                dots[i].xMove = '-';
+            }
+
+            if ((dots[i].x - dots[i].radius) <= 0) {
+                dots[i].xMove = '+';
+            }
+
+            if ((dots[i].y + dots[i].radius) >= cHeight) {
+                dots[i].yMove = '-';
+            }
+
+            if ((dots[i].y - dots[i].radius) <= 0) {
+                dots[i].yMove = '+';
+            }
+        }
+      
+        // Render it again
+        window.requestAnimationFrame(moveDot);
     }
+
+    // function to draw circular dot
+    function drawDot(dot) {
+        context.beginPath(); // begin drawing
+        context.arc(dot.x, dot.y, dot.radius, 0, 2 * Math.PI, false); // specify arc w/dot object's position and radius
+        context.fillStyle = dot.color; // add a color
+        context.fill(); // fill color in circle
+    }
+
+    // reset when the window is resized
+    window.addEventListener('resize', function() {
+        canvas.width  = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
+        cWidth = canvas.width;
+        cHeight= canvas.height;
+        context.clearRect(0, 0, cWidth, cHeight);
+        dots = [];
+        create();
+    });
 });
